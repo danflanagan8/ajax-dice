41e8a1a892d463277fbfaee939f3c626a00667e7
Dan Flanagan <dan.flanagan@manifest.com>
2018-08-13 13:48:44 -0500

    Change to an AppendCommand so that lots of dice can show. Change some names and generally clean up some stuff.

--- ajax_dice/js/ajaxDice.js
+++ ajax_dice/js/ajaxDice.js
@@ -2,9 +2,10 @@
 
   Drupal.behaviors.ajaxDice = {
     attach: function (context, settings) {
-      $('#ajax-dice-value', context).click(function(){
+      $('#ajax-dice-roller', context).click(function(){
         var rollDice = Drupal.ajax({
           url: Drupal.url('ajax_dice/roll'),
+          element: $('#ajax-dice-rolls'), //so that the throbber appears
         });
         rollDice.execute();
       });
--- ajax_dice/src/Controller/AjaxDiceController.php
+++ ajax_dice/src/Controller/AjaxDiceController.php
@@ -2,7 +2,7 @@
 namespace Drupal\ajax_dice\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\Core\Ajax\InsertCommand;
+use Drupal\Core\Ajax\AppendCommand;
 use Drupal\Core\Ajax\AjaxResponse;
 
 /**
@@ -11,18 +11,22 @@ use Drupal\Core\Ajax\AjaxResponse;
  * @package Drupal\ajax_dice\Controller
  */
 class AjaxDiceController extends ControllerBase {
+
   public function rollCallback(){
     $roll = $this->rollDice();
-    $response = new AjaxResponse();
     $content = [
       '#type' => 'markup',
       '#theme' => 'ajax_dice_roll',
       '#roll' => $roll,
     ];
-    $response->addCommand( new InsertCommand('#ajax-dice-value', $content));
+    $response = new AjaxResponse();
+    $response->addCommand( new AppendCommand('#ajax-dice-rolls', $content));
     return $response;
   }
 
+  /**
+   * Returns a random number between 1 and 6 using the random.org API
+   */
   protected function rollDice(){
     try {
       $number = file_get_contents('https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new');
@@ -34,5 +38,5 @@ class AjaxDiceController extends ControllerBase {
     }
     return $number;
   }
-  
+
 }
